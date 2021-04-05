e22b80e929c42d00b77c9b64c8f75d260e8945f8
Qu Wu <wuqu115@gmail.com>
2021-04-04 19:32:22 -0700

    m07assignment

---
+++ M07/parts/meta.php
@@ -0,0 +1,8 @@
+<!--<base href="http://hdraws.com/aau/wnm608_2/demo/m07">-->
+
+
+<meta name="viewport" content="width=device-width">
+
+<link rel="stylesheet" href="lib/css/styleguide.css">
+<link rel="stylesheet" href="lib/css/gridsystem.css">
+<link rel="stylesheet" href="css/storetheme.css">
