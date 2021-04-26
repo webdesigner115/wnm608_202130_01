ae7b08ee8d5ae0dd78d2974976551ee0d328913f
Qu Wu <wuqu115@gmail.com>
2021-04-25 19:19:54 -0700

    assignmentm08

---
+++ m08/product_list.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Candles Store Product List</title>
+	<? include "parts/meta.php" ?>
+</head>
+<body>
+	<? include "parts/navbar.php" ?>
+
+	<div class="container pad push-down">
+		<div class="grid gap xs-small md_medium product_list">
+			<div class="col-xs-6 col-md-4 col-lg-3">
+				<a href="product_item.php?id=1" class="product-item card card-soft card-light card-flat">
+					<div class="image-square">
+						<img src="https://via.placeholder.com/500/97c6c4/fff?text=Product 1" alt="product">
+					</div>
+					<div class="product-description">
+						<div>Product 1</div>
+						<div>$5.00</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-xs-6 col-md-4 col-lg-3">
+				<a href="product_item.php?id=2" class="product-item card card-soft card-light card-flat">
+					<div class="image-square">
+						<img src="https://via.placeholder.com/500/97c6c4/fff?text=Product 2" alt="product">
+					</div>
+					<div class="product-description">
+						<div>Product 2</div>
+						<div>$5.00</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-xs-6 col-md-4 col-lg-3">
+				<a href="product_item.php?id=3" class="product-item card card-soft card-light card-flat">
+					<div class="image-square">
+						<img src="https://via.placeholder.com/500/97c6c4/fff?text=Product 3" alt="product">
+					</div>
+					<div class="product-description">
+						<div>Product 3</div>
+						<div>$5.00</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-xs-6 col-md-4 col-lg-3">
+				<a href="product_item.php?id=4" class="product-item card card-soft card-light card-flat">
+					<div class="image-square">
+						<img src="https://via.placeholder.com/500/97c6c4/fff?text=Product 4" alt="product">
+					</div>
+					<div class="product-description">
+						<div>Product 4</div>
+						<div>$5.00</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-xs-6 col-md-4 col-lg-3">
+				<a href="product_item.php?id=5" class="product-item card card-soft card-light card-flat">
+					<div class="image-square">
+						<img src="https://via.placeholder.com/500/97c6c4/fff?text=Product 5" alt="product">
+					</div>
+					<div class="product-description">
+						<div>Product 5</div>
+						<div>$5.00</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-xs-6 col-md-4 col-lg-3">
+				<a href="product_item.php?id=6" class="product-item card card-soft card-light card-flat">
+					<div class="image-square">
+						<img src="https://via.placeholder.com/500/97c6c4/fff?text=Product 6" alt="product">
+					</div>
+					<div class="product-description">
+						<div>Product 6</div>
+						<div>$5.00</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-xs-6 col-md-4 col-lg-3">
+				<a href="product_item.php?id=7" class="product-item card card-soft card-light card-flat">
+					<div class="image-square">
+						<img src="https://via.placeholder.com/500/97c6c4/fff?text=Product 7" alt="product">
+					</div>
+					<div class="product-description">
+						<div>Product 7</div>
+						<div>$5.00</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-xs-6 col-md-4 col-lg-3">
+				<a href="product_item.php?id=8" class="product-item card card-soft card-light card-flat">
+					<div class="image-square">
+						<img src="https://via.placeholder.com/500/97c6c4/fff?text=Product 8" alt="product">
+					</div>
+					<div class="product-description">
+						<div>Product 8</div>
+						<div>$5.00</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-xs-6 col-md-4 col-lg-3">
+				<a href="product_item.php?id=9" class="product-item card card-soft card-light card-flat">
+					<div class="image-square">
+						<img src="https://via.placeholder.com/500/97c6c4/fff?text=Product 9" alt="product">
+					</div>
+					<div class="product-description">
+						<div>Product 9</div>
+						<div>$5.00</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-xs-6 col-md-4 col-lg-3">
+				<a href="product_item.php?id=10" class="product-item card card-soft card-light card-flat">
+					<div class="image-square">
+						<img src="https://via.placeholder.com/500/97c6c4/fff?text=Product 10" alt="product">
+					</div>
+					<div class="product-description">
+						<div>Product 10</div>
+						<div>$5.00</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-xs-6 col-md-4 col-lg-3">
+				<a href="product_item.php?id=11" class="product-item card card-soft card-light card-flat">
+					<div class="image-square">
+						<img src="https://via.placeholder.com/500/97c6c4/fff?text=Product 11" alt="product">
+					</div>
+					<div class="product-description">
+						<div>Product 11</div>
+						<div>$5.00</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-xs-6 col-md-4 col-lg-3">
+				<a href="product_item.php?id=12" class="product-item card card-soft card-light card-flat">
+					<div class="image-square">
+						<img src="https://via.placeholder.com/500/97c6c4/fff?text=Product 12" alt="product">
+					</div>
+					<div class="product-description">
+						<div>Product 12</div>
+						<div>$5.00</div>
+					</div>
+				</a>
+			</div>
+
+		</div>
+	</div>
+
+</body>
+</html>
